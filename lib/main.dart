ba25eb5b62c39e2ccfd5b3859c114864d7d7a5b4
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-11-03 22:00:52 +0530

    Design homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gutenberg_flutter/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,56 +12,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        accentColor: Color(0xFF5E56E7),
+        primaryColor: Color(0xFF5E56E7),
+        canvasColor: Color(0xFFF8F7FF),
+        cardColor: Color(0xFFFFFFFF),
+        appBarTheme: AppBarTheme(brightness: Brightness.light),
+        textTheme: TextTheme(
+          headline1: GoogleFonts.montserrat(
+            fontSize: 48,
+            fontWeight: FontWeight.w700,
+            // letterSpacing: 2.5,
+            color: Color(0xFF5E56E7),
+          ),
+          headline2: GoogleFonts.montserrat(
+              fontSize: 61, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+          headline3:
+              GoogleFonts.montserrat(fontSize: 48, fontWeight: FontWeight.w400),
+          headline4: GoogleFonts.montserrat(
+              fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+          headline5:
+              GoogleFonts.montserrat(fontSize: 24, fontWeight: FontWeight.w400),
+          headline6: GoogleFonts.montserrat(
+              fontSize: 20, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+          subtitle1: GoogleFonts.montserrat(
+              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+          subtitle2: GoogleFonts.montserrat(
+              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+          bodyText1: GoogleFonts.montserrat(
+              fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+          bodyText2: GoogleFonts.montserrat(
+              fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+          button: GoogleFonts.montserrat(
+              fontSize: 14, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+          caption: GoogleFonts.montserrat(
+              fontSize: 12, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+          overline: GoogleFonts.montserrat(
+              fontSize: 10, fontWeight: FontWeight.w400, letterSpacing: 1.5),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ lib/widgets/genre_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class GenreCard extends StatelessWidget {
+  final String genreName;
+  final String iconPath;
+  const GenreCard({
+    @required this.genreName,
+    @required this.iconPath,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(
+        vertical: 8.0,
+        horizontal: 16.0,
+      ),
+      borderOnForeground: true,
+      shadowColor: Color.fromRGBO(211, 209, 238, 0.5),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: ListTile(
+        leading: SvgPicture.asset(
+          iconPath,
+          fit: BoxFit.contain,
+          height: 30.0,
+        ),
+        title: Text(
+          genreName,
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
+        ),
+        trailing: SvgPicture.asset('assets/Next.svg'),
+      ),
+    );
+  }
+}
