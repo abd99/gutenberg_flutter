4008ae0d87fe98e30d658191f3e4578fc589414b
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-11-05 00:59:53 +0530

    Make HomePage adaptive to screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -22,46 +22,48 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Stack(
-            children: [
-              SvgPicture.asset(
-                'assets/Pattern.svg',
-                fit: BoxFit.cover,
-                height: 350,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 128.0,
-                  left: 16.0,
-                  right: 16.0,
-                  bottom: 16.0,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Stack(
+              children: [
+                SvgPicture.asset(
+                  'assets/Pattern.svg',
+                  fit: BoxFit.cover,
+                  height: 350,
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      'Gutenberg Project',
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Text(
-                      kAppDescription,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 128.0,
+                    left: 16.0,
+                    right: 16.0,
+                    bottom: 16.0,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Gutenberg Project',
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Text(
+                        kAppDescription,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 8.0),
-          Column(
-            children: genreCards,
-          )
-        ],
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Column(
+              children: genreCards,
+            )
+          ],
+        ),
       ),
     );
   }
