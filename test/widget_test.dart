d597fc491443213c3ffbce3243513f1d0c98ba7d
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-11-05 00:16:21 +0530

    Fix Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:gutenberg_flutter/main.dart';
+import 'package:gutenberg_flutter/widgets/genre_card.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Gutenberg());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byType(MaterialApp), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.byType(GenreCard).first);
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(Scaffold), findsOneWidget);
   });
 }
